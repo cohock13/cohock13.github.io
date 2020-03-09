20bface8f0a4546cc1a15c227b02f070e5cd4b68
cohock <callout2690.gmail.com>
2020-03-09 16:51:26 +0900

    hogehoge

--- sketch.js
+++ sketch.js
@@ -1,12 +1,33 @@
+var leftBuffer;
+var rightBuffer;
+
 function setup() {
-    createCanvas(640, 480);
-  }
-  
-  function draw() {
-    if (mouseIsPressed) {
-      fill(0);
-    } else {
-      fill(255);
-    }
-    ellipse(mouseX, mouseY, 80, 80);
-  }
+    // 800 x 400 (double width to make room for each "sub-canvas")
+    createCanvas(800, 400);
+    // Create both of your off-screen graphics buffers
+    leftBuffer = createGraphics(400, 400);
+    rightBuffer = createGraphics(400, 400);
+}
+
+function draw() {
+    // Draw on your buffers however you like
+    drawLeftBuffer();
+    drawRightBuffer();
+    // Paint the off-screen buffers onto the main canvas
+    image(leftBuffer, 0, 0);
+    image(rightBuffer, 400, 0);
+}
+
+function drawLeftBuffer() {
+    leftBuffer.background(0, 0, 0);
+    leftBuffer.fill(255, 255, 255);
+    leftBuffer.textSize(32);
+    leftBuffer.text("This is the left buffer!", 50, 50);
+}
+
+function drawRightBuffer() {
+    rightBuffer.background(255, 100, 255);
+    rightBuffer.fill(0, 0, 0);
+    rightBuffer.textSize(32);
+    rightBuffer.text("This is the right buffer!", 50, 50);
+}
