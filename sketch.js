4b057a0999aaf285fc8db221e9abcdb86ae5480b
cohock <callout2690.gmail.com>
2020-03-09 18:05:37 +0900

    responsive

--- sketch.js
+++ sketch.js
@@ -1,33 +1,38 @@
-var leftBuffer;
-var rightBuffer;
-
-function setup() {
-    // 800 x 400 (double width to make room for each "sub-canvas")
-    createCanvas(windowWidth, windowHeight);
-    // Create both of your off-screen graphics buffers
-    leftBuffer = createGraphics(windowWidth/2, windowHeight/2);
-    rightBuffer = createGraphics(windowWidth/2, windowHeight/2);
-}
-
-function draw() {
-    // Draw on your buffers however you like
-    drawLeftBuffer();
-    drawRightBuffer();
-    // Paint the off-screen buffers onto the main canvas
-    image(leftBuffer, 0, 0);
-    image(rightBuffer, 400, 0);
-}
-
-function drawLeftBuffer() {
-    leftBuffer.background(0, 0, 0);
-    leftBuffer.fill(255, 255, 255);
-    leftBuffer.textSize(32);
-    leftBuffer.text("This is the left buffer!", 50, 50);
-}
-
-function drawRightBuffer() {
-    rightBuffer.background(255, 100, 255);
-    rightBuffer.fill(0, 0, 0);
-    rightBuffer.textSize(32);
-    rightBuffer.text("This is the right buffer!", 50, 50);
-}
+// save this file as sketch.js
+// Sketch One
+var s = function( p ) { // p could be any variable name
+    var x = 100; 
+    var y = 100;
+    p.setup = function() {
+      p.createCanvas(window.innerWidth/2*0.95,window.innerHeight);
+    };
+  
+    p.draw = function() {
+      p.background(0);
+      p.fill(255);
+      p.rect(x,y,50,50);
+    };
+  };
+  var myp5 = new p5(s, 'c1');
+  
+  // Sketch Two
+  var t = function( p ) { 
+    var x = 100.0; 
+    var y = 100; 
+    var speed = 2.5; 
+    p.setup = function() {
+      p.createCanvas(window.innerWidth/2*0.95,window.innerHeight);
+    };
+  
+    p.draw = function() {
+      p.background(100);
+      p.fill(1);
+      x += speed; 
+      if(x > p.width){
+        x = 0; 
+      }
+      p.ellipse(x,y,50,50);
+  
+    };
+  };
+  var myp5 = new p5(t, 'c2');
