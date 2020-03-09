d367527c62e2531e600db3bbec8d1bd52158e83d
cohock <callout2690.gmail.com>
2020-03-09 16:31:52 +0900

    hogee

--- sketch.js
+++ sketch.js
@@ -1,7 +1,12 @@
 function setup() {
-
-}
-
-function draw() {
-  ellipse(50, 50, 80, 80);
-}
+    createCanvas(640, 480);
+  }
+  
+  function draw() {
+    if (mouseIsPressed) {
+      fill(0);
+    } else {
+      fill(255);
+    }
+    ellipse(mouseX, mouseY, 80, 80);
+  }
