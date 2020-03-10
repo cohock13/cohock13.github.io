a58772605b4c57b286c1ae88647e5954e31d03c5
cohock <callout2690@gmail.com>
2020-03-11 07:25:24 +0900

    scroll

--- sketch.js
+++ sketch.js
@@ -36,3 +36,48 @@ var s = function( p ) { // p could be any variable name
     };
   };
   var myp5 = new p5(t, 'c2');
+
+    // Sketch Two
+    var t = function( p ) { 
+        var x = 100.0; 
+        var y = 100; 
+        var speed = 2.5; 
+        p.setup = function() {
+          p.createCanvas(window.innerWidth/2*0.5,window.innerHeight*0.4);
+        };
+      
+        p.draw = function() {
+          p.background(100);
+          p.fill(1);
+          x += speed; 
+          if(x > p.width){
+            x = 0; 
+          }
+          p.ellipse(x,y,50,50);
+      
+        };
+      };
+      var myp5 = new p5(t, 'c3');
+
+
+        // Sketch Two
+  var t = function( p ) { 
+    var x = 100.0; 
+    var y = 100; 
+    var speed = 2.5; 
+    p.setup = function() {
+      p.createCanvas(window.innerWidth/2*0.5,window.innerHeight*0.4);
+    };
+  
+    p.draw = function() {
+      p.background(100);
+      p.fill(1);
+      x += speed; 
+      if(x > p.width){
+        x = 0; 
+      }
+      p.ellipse(x,y,50,50);
+  
+    };
+  };
+  var myp5 = new p5(t, 'c4');
