703f8ccc6f5c3f4938e7ce77a6a87170c20d5091
cohock13 <callout2690@gmail.com>
2020-06-30 22:37:37 +0900

    a

--- models/Nagel-Schreckenberg/tmp/sketch.js
+++ models/Nagel-Schreckenberg/tmp/sketch.js
@@ -4,51 +4,64 @@ https://p5js.org/examples/simulate-wolfram-ca.html
 */
 let cells;
 let param;
-
-let ruleset = [0, 1, 0, 1, 1, 0, 1, 0];
-
+let velocity;
+let index;
 function paramters() {
-  this.w = 10;
+  this.w = 5;
   this.width_ = 10;
   this.generation = 0;
+  this.v_max = 2;
+  this.p = 0;
   this.reset = function() {
     init();
     clear();
-    background(230);
+    background(255);
   }
 }
 
 function init(){
   param.w = param.width_;
   cells = Array(floor(windowWidth/param.w));
+  //要素が2以下だと後で困るので
+  cells[0] = 1;
+  cells[floor(cells.length/2)] = 1;
   //乱数でcellsの01を決定
   for(let i = 0;i < cells.length;++i){
-    cells[i] = Math.round(random());
+    cells[i] = Math.round(random(min(0.5+0.1*param.w,0.7)));
+  }
+  index = Array();
+  velocity = Array();
+  //cellsの1の部分のindexを取る､|vel|=|index|
+  for(let i = 0;i < cells.length; ++i){
+    if(cells[i] === 1){
+      append(index,i);
+      append(velocity,0);
+    }
   }
   param.generation = 0;
-
-  //clear();
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight*0.9);
+  resizeCanvas(windowWidth, windowHeight*0.95);
 }
 
 function setup() {
-  createCanvas(windowWidth,windowHeight*0.9);
+  createCanvas(windowWidth,windowHeight*0.95);
   colorMode(RGB);
-  background(230);
+  background(255);
   param = new paramters();
   init();
   let gui = new dat.GUI();
   gui.add(param,"width_",0.5,10).step(0.5);
+  gui.add(param,"v_max",0,10).step(1);
+  gui.add(param,"p",0,1).step(0.1);
   gui.add(param,"reset");
 }
 
 function draw() {
   for (let i = 0; i < cells.length; i++) {
     if (cells[i] === 1) {
-      fill(230);
+      fill(0);
     } 
     else{
       fill(40);
@@ -56,39 +69,45 @@ function draw() {
       rect(i * param.w, param.generation*param.w, param.w, param.w);
     }
   }
-  if (param.generation < windowHeight*0.9/param.w) {
-    generate();
+  if (param.generation < windowHeight*0.95/param.w) {
+    update_cells();
   }
 }
 
-// The process of creating the new generation
-function generate() {
-  // First we create an empty array for the new values
-  let nextgen = Array(cells.length);
-  // For every spot, determine new state by examing current state, and neighbor states
-  // Ignore edges that only have one neighor
-  for (let i = 1; i < cells.length-1; i++) {
-    let left   = cells[i-1];   // Left neighbor state
-    let me     = cells[i];     // Current state
-    let right  = cells[i+1];   // Right neighbor state
-    nextgen[i] = rules(left, me, right); // Compute next generation state based on ruleset
+function update_cells() {
+  for(let i = 0; i < len.cells;++i){
+    cells[i] = 0;
   }
-  // The current generation is the new generation
-  cells = nextgen;
-  param.generation++;
-}
+  let len = velocity.length;
+  let dist = Array(len);
+  //dist配列を計算
+  for(let i = 0; i < len; ++i){
+    if(i === len-1){
+      append(dist,(len-index[i]-1+len[0]));
+    }
+    else{
+      append(dist,index[i+1]-index[i]);
+    }
+  }
+  //vの更新
+  for(let i = 0; i < len; ++i){
+    let v = min(param.v_max,velocity[i]+1);
+    v = min(v,dist[i]-1);
+    let p_ = random();
+    if(p_ < param.p){
+      v = max(v-1,0);
+    }
+    
+    if(index[i]+v < len){
+      index[i] += v;
+    }
+    else{
+      index[i] += v - len;
+    }
+    velocity[i] = v;
+  }
 
-
-// Implementing the Wolfram rules
-// Could be improved and made more concise, but here we can explicitly see what is going on for each case
-function rules(a, b, c) {
-  if (a == 1 && b == 1 && c == 1) return ruleset[0];
-  if (a == 1 && b == 1 && c == 0) return ruleset[1];
-  if (a == 1 && b == 0 && c == 1) return ruleset[2];
-  if (a == 1 && b == 0 && c == 0) return ruleset[3];
-  if (a == 0 && b == 1 && c == 1) return ruleset[4];
-  if (a == 0 && b == 1 && c == 0) return ruleset[5];
-  if (a == 0 && b == 0 && c == 1) return ruleset[6];
-  if (a == 0 && b == 0 && c == 0) return ruleset[7];
-  return 0;
+  for(let i = 0; i < len ; ++i){
+    cells[index[i]] = 1;
+  }
 }
