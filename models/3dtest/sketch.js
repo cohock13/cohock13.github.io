2893700ba6b0d8eaa21193d03ffec5864d838a77
cohock13 <callout2690@gmail.com>
2021-04-28 02:04:49 +0900

    a

---
+++ models/3dtest/sketch.js
@@ -0,0 +1,16 @@
+let modelData;
+let camera;
+
+function preload(){
+    modelData = loadModel('/models/3dtest/car.obj');
+}
+function setup(){
+
+	createCanvas(windowWidth*0.9,windowHeight*0.9,WEBGL);
+    camera = createEasyCam({distance:windowWidth});
+
+}
+
+function draw(){
+    model(modelData);
+}
