84223032dcf28e11fa67996fcdf4e25bcc1bde5a
cohock13 <55901554+cohock13@users.noreply.github.com>
2021-04-28 02:53:58 +0900

    Update sketch.js

--- models/3dtest/sketch.js
+++ models/3dtest/sketch.js
@@ -5,7 +5,7 @@ function preload(){
     modelData = loadModel('/models/3dtest/car.obj',true);
 }
 function setup(){
-
+	camera = createEasyCam();
 	createCanvas(windowWidth*0.9,windowHeight*0.9,WEBGL);
 
 }
