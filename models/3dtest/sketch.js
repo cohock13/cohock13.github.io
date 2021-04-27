d9a1aea809c3ead9fe527591d9c4ffacc8277eab
cohock13 <55901554+cohock13@users.noreply.github.com>
2021-04-28 03:03:39 +0900

    Update sketch.js

--- models/3dtest/sketch.js
+++ models/3dtest/sketch.js
@@ -5,13 +5,10 @@ function preload(){
     modelData = loadModel('/models/3dtest/car.obj');
 }
 function setup(){
-<<<<<<< HEAD
+
     img = loadImage("/models/3dtest/color.PNG");
-=======
-	camera = createEasyCam();
->>>>>>> b575230898ae23d919c2f88c26c9cdf4ee5d673d
-	createCanvas(windowWidth*0.9,windowHeight*0.9,WEBGL);
     camera = createEasyCam();
+    createCanvas(windowWidth*0.9,windowHeight*0.9,WEBGL);
 }
 
 function draw(){
