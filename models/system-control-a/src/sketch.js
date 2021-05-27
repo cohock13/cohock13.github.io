8020aef4824dba99204139e628c326d1959a05ef
cohock13 <callout2690@gmail.com>
2021-05-27 12:31:12 +0900

    fixed sketch.js

--- models/system-control-a/src/sketch.js
+++ models/system-control-a/src/sketch.js
@@ -17,7 +17,7 @@ function setup(){
 	let gui = new dat.GUI();
 
 	gui.add(param,"omega",0,3,0.001).listen();
-	gui.add(param,"zeta",0,3,0.001).listen();
+	gui.add(param,"zeta",0,2,0.001).listen();
 	gui.open();
 	
 }
@@ -32,14 +32,14 @@ function draw(){
 
 let time = 100; // range of x, [0,x]
 let step = 10000;
-let xval = 2; // range of y, [-y,y]
+let xval = 2.3; // range of y, [-y,y]
 
 // graph curve
 function drawFunction(){
 
-	let left = 0.1*windowWidth;
+	let left = 0.2*windowWidth;
 	let right_ = windowWidth;
-	let top_ = 0.1*windowHeight;
+	let top_ = 100;
 	let bot = 0.9*windowHeight;
 
 
@@ -76,20 +76,37 @@ function drawFunction(){
 	let width_ = (right_-left)/step;
 
 	stroke(255,0,0);
-	line(0,height_,windowWidth,height_);
+	line(0,height_+top_,windowWidth,height_+top_);
+	stroke(51);
+	strokeWeight(3);
+	line(0,height_*2+top_,left,height_*2+top_);
+	stroke(150);
+	strokeWeight(2);
+	line(left,0,left,windowHeight); // y axis
+	line(0,height_*2+top_,windowWidth,height_*2+top_); // x axis
+
+	textSize(40);
+	noStroke();
+	fill(150);
+	text("0",left-0.03*windowWidth,height_*2+top_-0.02*windowWidth);
+	fill(255,0,0);
+	text("1",left-0.03*windowWidth,height_+top_-0.02*windowHeight);
+	fill(10);
+	text("x(t)",left-0.05*windowWidth,top_);
+	text("t",windowWidth*0.5,height_*2+top_+0.05*windowHeight);
+
 
 
 	for(let i = 0; i < step; ++i){
 		
 		//draw line
-		let x1 = width_*i;
-		let y1 = height_*(2-func(t));
-		let x2 = width_*(i+1);
-		let y2 = height_*(2-func(t+dt));
+		let x1 = left+width_*i;
+		let y1 = height_*(2-func(t))+top_;
+		let x2 = left+width_*(i+1);
+		let y2 = height_*(2-func(t+dt))+top_;
 		stroke(51);
 		line(x1,y1,x2,y2);
-		stroke(100);
-		strokeWeight(3);
+
 
 		t += dt;
 
