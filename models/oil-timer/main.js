ce81eb26e0d514da07960501c32f5cff5ac12d46
syamaji <callout2690@gmail.com>
2025-07-24 03:31:47 +0900

    oil-timer : deleted tmp files.

--- models/oil-timer/main.js
+++ models/oil-timer/main.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 import { OilTimer } from './oil-timer.js';
 
 // Initialize the mochi oil timer when the page loads
 window.addEventListener('load', () => {
     new OilTimer();
-=======
-import { OilTimer } from './oiltimer.js';
-
-// Initialize the oil timer when the page loads
-window.addEventListener('load', () => {
-    new OilTimer();
->>>>>>> c6a203ca60b87e0942b52b11d77698cd20b9a36b
 });
