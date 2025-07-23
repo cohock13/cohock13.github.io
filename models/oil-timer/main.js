af3e58701fe995b9fce4d15f84ff1b2d40933c7c
syamaji <callout2690@gmail.com>
2025-07-24 03:24:23 +0900

    oil-tiler : fixed gui.

---
+++ models/oil-timer/main.js
@@ -0,0 +1,6 @@
+import { OilTimer } from './oil-timer.js';
+
+// Initialize the mochi oil timer when the page loads
+window.addEventListener('load', () => {
+    new OilTimer();
+});
--- models/oil_timer/main.js
+++
@@ -1,6 +0,0 @@
-import { OilTimer } from './oiltimer.js';
-
-// Initialize the oil timer when the page loads
-window.addEventListener('load', () => {
-    new OilTimer();
-});
